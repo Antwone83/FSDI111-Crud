91dcf5d37a1c251e9a79df96fbe344cbfeb32bf8
Antwone Adams <adams.antwone@yahoo.com>
2022-02-09 17:03:32 -0800

    Submitting work

---
+++ utils/update_user.py
@@ -0,0 +1,66 @@
+from pprint import pprint
+import requests
+
+
+URL = "http://127.0.0.1:/5000/users"
+
+EXAMPLE_JSON = {
+    "first_name": "Tony",
+    "last_name": "Smith",
+    "hobbies": "Sleeping"
+}
+
+
+def display_user(pk, first_name, last_name, hobbies):
+    user_json = EXAMPLE_JSON
+    user_json["first_name"] = first_name
+    user_json["last_name"] = last_name
+    user_json["hobbies"] = hobbies
+    current_user = requests.get("%s/%s" % (URL, pk))
+    if current_user.status_code == 200:
+        pprint(current_user.json())
+    else:
+        print(
+            "Something went wrong while "
+            "trying to display the user.")
+    out = requests.put("%s/%s" % (URL, pk) ,json=user_json)
+    if out.status_code == 204:
+        print("Change successful.")
+    else:
+        print(
+            "Something went wrong while"
+            "trying to update the user.")
+
+
+def update_user(pk, first_name, last_name, hobbies):
+    user_json = EXAMPLE_JSON
+    user_json["first_name"] = first_name
+    user_json["last_name"] = last_name
+    user_json["hobbies"] = hobbies
+    current_user = requests.get("%s/%s" % (URL, pk))
+    if current_user.status_code == 200:
+        pprint(current_user.json())
+    else:
+        print(
+            "Something went wrong while "
+            "trying to display the user.")
+    out = requests.put("%s/%s" % (URL, pk) ,json=user_json)
+    if out.status_code == 204:
+        print("Change successful.")
+    else:
+        print(
+            "Something went wrong while"
+            "trying to update the user.")
+
+
+if __name__ == "__main__":
+    pk = int(input("Type in the desired user id:  "))
+    print("Current User")
+    display_user(pk)
+    first_name = input("Type in the desired first name: ")
+    last_name = input("Type in the desired last name: ")
+    hobbies = input("Type in the desired hobbies: ")
+    update_user(pk, first_name, last_name, hobbies)
+    option = input("Would you like to display the changes? [y/n]")
+    if option == "y" or option == "Y":
+        display_user(pk)
